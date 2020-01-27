83549c36ce245b2fc0a10e11e8855797b64d97de
Bublade <alexthvb@gmail.com>
2020-01-27 20:37:05 +0100

    replacing '.js' also migth make it better :)

--- minify.sh
+++ minify.sh
@@ -6,7 +6,7 @@ echo Creating psmin file: $filepath
 
 uglifyjs js/*.js js/*/*.js js/*/*/*.js -o $filepath
 
-sed -i -E "s@(psmin_)[0-9\-]+@$filepath@g" index.html
+sed -i -E "s@(psmin_)[0-9\-]+\.js@$filepath@g" index.html
 cp index.html test.html
 
 echo Done
