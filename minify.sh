ef419befaf8e6dc6547e3df8d80361f4e48ea25a
Alastair-L <lockealastair@gmail.com>
2020-08-30 21:47:32 +1000

    Add yarn command

--- minify.sh
+++ minify.sh
@@ -4,7 +4,7 @@ timestamp=$(date "+%Y%m%d-%H%M%S")
 filepath="psmin_$timestamp.js"
 echo Creating psmin file: $filepath
 
-uglifyjs js/*.js js/*/*.js js/*/*/*.js -o $filepath
+babel js/*.js js/*/*.js js/*/*/*.js -o $filepath
 
 sed -i.bu -E "s/(psmin_)[0-9\-]+\.js/$filepath/g" index.html
 cp index.html test.html
