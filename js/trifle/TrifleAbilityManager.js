3766a8f3bf819d22c277ff27ed7c95040efb14f3
Zach Burnham <thejambi@gmail.com>
2022-02-17 23:24:02 -0500

    Ginseng bug fix by uncommenting important looking code!

--- js/trifle/TrifleAbilityManager.js
+++ js/trifle/TrifleAbilityManager.js
@@ -51,11 +51,11 @@ Trifle.AbilityManager.prototype.activateReadyAbilities = function() {
 	/* Deactivate abilities. New ability list is the ones that are not deactivated. */
 	var newAbilities = [];
 	this.abilities.forEach(function(existingAbility) {
-		if (existingAbility.preserve && !self.abilityIsCanceled(existingAbility)) {
-			newAbilities.push(existingAbility);
-		} else {
+		// if (existingAbility.preserve && !self.abilityIsCanceled(existingAbility)) {
+		// 	newAbilities.push(existingAbility);
+		// } else {
 			existingAbility.deactivate();
-		}
+		// }
 	});
 	this.abilities = newAbilities;
 
